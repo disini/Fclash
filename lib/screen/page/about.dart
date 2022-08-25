e70de7385a797c61212f7b35ec80e4ad62a52f10
Kingtous <kingtous@qq.com>
2022-08-25 10:20:44 +0800

    bump 1.2.6-2
    Signed-off-by: Kingtous <kingtous@qq.com>

--- lib/screen/page/about.dart
+++ lib/screen/page/about.dart
@@ -29,7 +29,7 @@ class AboutPage extends StatelessWidget {
             ),
           ),
           Text(
-            "version:".trParams({"version": '1.2.5-1'}),
+            "version:".trParams({"version": '1.2.6-2'}),
             style: const TextStyle(fontFamily: 'nssc'),
           ),
           TextButton(
