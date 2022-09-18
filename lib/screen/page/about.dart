32b0048fb6acb0dffc2942ab8f5cfeb8cb0b4e35
Kingtous <kingtous@qq.com>
2022-09-18 11:19:28 +0800

    bump: 1.3.2

--- lib/screen/page/about.dart
+++ lib/screen/page/about.dart
@@ -29,7 +29,7 @@ class AboutPage extends StatelessWidget {
             ),
           ),
           Text(
-            "version:".trParams({"version": '1.3.1'}),
+            "version:".trParams({"version": '1.3.2'}),
             style: const TextStyle(fontFamily: 'nssc'),
           ),
           TextButton(
