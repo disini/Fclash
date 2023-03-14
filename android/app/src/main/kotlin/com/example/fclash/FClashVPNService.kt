3e39e83576606770ba42312733bb72ec5c21e0e2
Kingtous <kingtous@qq.com>
2023-03-14 15:47:32 +0800

    feat: try impl fclash vpnservice

--- android/app/src/main/kotlin/com/example/fclash/FClashVPNService.kt
+++ android/app/src/main/kotlin/com/example/fclash/FClashVPNService.kt
@@ -1,13 +1,14 @@
 package com.example.fclash
 
 import android.content.Intent
-import android.content.IntentFilter
-import android.net.ConnectivityManager
-import android.net.InetAddresses
 import android.net.VpnService
-import java.net.InetAddress
+import android.os.ParcelFileDescriptor
+import android.util.Log
+import java.io.FileInputStream
+import java.io.OutputStream
+import java.net.Socket
 
-class FClashVPNService: VpnService() {
+class FClashVPNService : VpnService() {
 
     companion object {
         const val TAG = "FClashPlugin"
@@ -17,16 +18,48 @@ class FClashVPNService: VpnService() {
         }
     }
 
-    override fun onCreate() {
-        super.onCreate()
-        val ifConnectivity = IntentFilter()
-        ifConnectivity.addAction(ConnectivityManager.CONNECTIVITY_ACTION)
-        Builder().addAddress(InetAddress.)
-    }
-
+    private var httpThread: Thread? = null
+    private var mFd: ParcelFileDescriptor? = null
+    private var clashSocket: Socket? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        return super.onStartCommand(intent, flags, startId)
+        mFd = with(Builder()) {
+            addAddress("10.0.0.2", 32)
+            addRoute("0.0.0.0", 0)
+            addDnsServer("8.8.8.8")
+            setMtu(1500)
+            setSession("FClash")
+            establish()
+        }
+        if (mFd == null) {
+            Log.e("FClash", "Interface creation failed")
+            return START_NOT_STICKY
+        }
+        httpThread = Thread {
+            startHttpService()
+        }
+        httpThread?.start()
+        return START_STICKY
+    }
+    private fun startHttpService() {
+        clashSocket = Socket("127.0.0.1", 7890)
+        protect(clashSocket)
+        val stream = FileInputStream(mFd?.fileDescriptor)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopVpnService()
+    }
+
+    private fun stopVpnService() {
+        try {
+            httpThread?.interrupt()
+            // Close the VPN interface
+            mFd?.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
 }
--- android/app/src/main/kotlin/com/example/fclash/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/fclash/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.example.fclash
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.VpnService
+import android.os.Build
+import android.widget.Toast
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity(), MethodChannel.MethodCallHandler {
+
+class MainActivity : FlutterActivity(), MethodChannel.MethodCallHandler {
     companion object {
         lateinit var flutterMethodChannel: MethodChannel
     }
+
+    private var isRunning = false
+    private val VPN_PERMISSION_REQUEST_CODE = 1001
 
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
@@ -18,17 +28,77 @@ class MainActivity: FlutterActivity(), MethodChannel.MethodCallHandler {
     }
 
     override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
-        when(call.method) {
+        when (call.method) {
             FClashVPNService.Companion.Action.StartProxy.toString() -> {
-
+                startVpnService()
                 result.success(null)
             }
             FClashVPNService.Companion.Action.StopProxy.toString() -> {
+                stopVpnService()
                 result.success(null)
             }
             else -> {
                 result.notImplemented()
             }
+        }
+    }
+
+    private fun startVpnService() {
+        // Start the VPN service
+        val intent = Intent(this, FClashVPNService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent)
+        } else {
+            startService(intent)
+        }
+        isRunning = true
+        // Request VPN permission
+        if (!hasVpnPermission()) {
+            val vpnPermissionIntent = VpnService.prepare(this)
+            if (vpnPermissionIntent != null) {
+                startActivityForResult(vpnPermissionIntent, VPN_PERMISSION_REQUEST_CODE)
+            } else {
+                onVpnPermissionResult(RESULT_OK)
+            }
+        } else {
+            onVpnPermissionResult(RESULT_OK)
+        }
+    }
+
+    private fun stopVpnService() {
+        if (isRunning) {
+            val intent = Intent(this, FClashVPNService::class.java)
+            stopService(intent)
+            isRunning = false
+        }
+    }
+
+    private fun onVpnPermissionResult(resultCode: Int) {
+        if (resultCode == RESULT_OK) {
+            // VPN permission granted
+            Toast.makeText(this, "已授予VPN权限", Toast.LENGTH_SHORT).show()
+        } else {
+            // VPN permission denied
+            Toast.makeText(this, "VPN权限申请被拒绝", Toast.LENGTH_SHORT).show()
+            stopVpnService()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == VPN_PERMISSION_REQUEST_CODE) {
+            onVpnPermissionResult(resultCode)
+        }
+    }
+
+    private fun hasVpnPermission(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            (checkSelfPermission(Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) &&
+                    (checkSelfPermission(Manifest.permission.RECEIVE_BOOT_COMPLETED) == PackageManager.PERMISSION_GRANTED) &&
+                    (checkSelfPermission(Manifest.permission.FOREGROUND_SERVICE) == PackageManager.PERMISSION_GRANTED)
+        } else {
+            (checkSelfPermission(Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) &&
+                    (checkSelfPermission(Manifest.permission.RECEIVE_BOOT_COMPLETED) == PackageManager.PERMISSION_GRANTED)
         }
     }
 }
